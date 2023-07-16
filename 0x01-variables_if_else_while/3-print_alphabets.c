75dd919f3b4b0e63be0747942e5d2bc5db72bc6a
Adilha-codes <adil.hassani.idrissi1@gmail.com>
2023-07-16 09:12:56 -0700

    er

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: alphabet
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	while (CH <= 'Z')
+        {
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}
