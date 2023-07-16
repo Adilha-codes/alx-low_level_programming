89168feaa33742e6ef729d6526628aab9ec2429f
Adilha-codes <adil.hassani.idrissi1@gmail.com>
2023-07-16 09:56:04 -0700

    er

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: reverse alp
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+	
+	while (ch >= 'a')
+	{
+	    putchar('\n');
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
